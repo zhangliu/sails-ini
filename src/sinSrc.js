f7bb3967b91b4b17bf50989433a94446abe0ce84
yunnian.zhang <yunnian.zhang@yintech.cn>
2018-04-02 16:03:40 +0800

    modify code

--- src/sinSrc.js
+++ src/sinSrc.js
@@ -2,18 +2,20 @@ const fs = require('fs')
 const _ = require('lodash')
 const colors = require('colors')
 
-if (!fs.existsSync('./package.json')) {
-  console.log(colors.yellow('[warning]: please run in a sails project folder'))
+const packageStr = fs.readFileSync('./package.json')
+const packageInfo = JSON.parse(packageStr || '')
+
+const sailsVersion = _.get(packageInfo, 'dependencies.sails')
+if (!sailsVersion) {
+  console.log(colors.red('[error]: please run in a sails project folder'))
   process.exit()
 }
 
-const packageStr = fs.readFileSync('./package.json')
-const packageInfo = JSON.parse(packageStr)
+console.log(`获取到sails版本信息：${sailsVersion}`)
 
-const sailsVersion = _.get(packageInfo, 'dependencies.sails', null)
 if (/^[^\d]*1\..*/.test(sailsVersion)) { // 1.0 版本
   process.exit()
 }
 
 // 0.12版本
-console.log(sailsVersion, '---------------')
+fs.readFileSync(`${__dirname}/`)
