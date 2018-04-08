bbd8a0bc4eadc3ed5d35487cc50dc9b3020e1e0f
yunnian.zhang <yunnian.zhang@yintech.cn>
2018-04-08 11:14:14 +0800

    modify

--- src/template/v0.12.x/config/routes.js
+++ src/template/v0.12.x/config/routes.js
@@ -26,45 +26,5 @@ module.exports.routes = {
    * 前台路由
    */
   'get /': (req, res) => res.ok('ok'),
-  'get /health': (req, res) => res.ok('ok'),
-
-  // 比赛相关
-  'get /game': 'GameController.findGame',
-
-  // 获取比赛全量数据
-  'get /game/populate': 'GameController.findByGameId',
-
-  // 用户下注订单操作
-  'get /betOrderByUserId': 'BetOrderController.getBetOrderByUserId',
-  'post /betOrder': 'BetOrderController.postBetOrder',
-
-  // 微信支付充值接口
-  'post /wxpay': 'WxpayController.createWxpay',
-  'post /wxpay/notify': 'WxpayController.notifyCallback',
-
-  /**
-   * 管理后台路由
-   */
-  'get /admin/game': 'GameController.findGame',
-  'put /admin/game/:id': 'GameController.update',
-
-  // 获取单个比赛相关数据接口
-  'get /admin/gameinfo': 'GameController.findByGameId',
-
-  // 比赛盘口创建接口
-  'post /admin/game': 'GameController.create',
-
-  // 创建盘口
-  'post /admin/handicap': 'HandicapController.create',
-
-  // 修改盘口数据
-  'put /admin/handicap/:id': 'HandicapController.update',
-
-  'get /admin/settlement': 'SettlementController.findSettlementResult',
-
-  // 结算相关
-  'post /admin/settlement': 'SettlementController.close',
-
-  // 盘口模板相关
-  'get /admin/handicapTemplate': 'HandicapTemplateController.find'
+  'get /health': (req, res) => res.ok('ok')
 }
